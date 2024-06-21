090f4a88578cd71c2f57ff220b1aefea3d24f59a
Jesse Swart <jesseswart08@gmail.com>
2024-06-21 14:44:01 +0200

    Expanded the codex a bit more

--- utils/encode_char.py
+++ utils/encode_char.py
@@ -61,27 +61,61 @@ __codex = {
     '7': (0,0,59),
     '8': (0,0,60),
     '9': (0,0,61),
-    ' ': (0,0,62)
+    ' ': (0,0,62),
+    '!': (0,0,63),
+    '@': (0,0,64),
+    '#': (0,0,65),
+    '$': (0,0,66),
+    '%': (0,0,67),
+    '^': (0,0,68),
+    '&': (0,0,69),
+    '*': (0,0,70),
+    '(': (0,0,71),
+    ')': (0,0,72),
+    '-': (0,0,73),
+    '_': (0,0,74),
+    '=': (0,0,75),
+    '+': (0,0,76),
+    '[': (0,0,77),
+    ']': (0,0,78),
+    '{': (0,0,79),
+    '}': (0,0,80),
+    '|': (0,0,81),
+    '\\': (0,0,82),
+    ';': (0,0,83),
+    ':': (0,0,84),
+    '\'': (0,0,85),
+    '"': (0,0,86),
+    ',': (0,0,87),
+    '.': (0,0,88),
+    '<': (0,0,89),
+    '>': (0,0,90),
+    '/': (0,0,91),
+    '?': (0,0,92),
+    '`': (0,0,93),
+    '~': (0,0,94),
 }
 
 def get_codex() -> dict:
     return __codex
 
-def __get_values():
-    return __codex.values()
+def __get_values() -> list:
+    return list(__codex.values())
 
-def __get_keys():
-    return __codex.keys()
+def __get_keys() -> list:
+    return list(__codex.keys())
 
 def get_key_from_value(value):
-    if value in __get_values():
-        for key, val in __codex.items():
-            if val == value:
-                return key
+    if value not in __get_values():
+        return ('|CharNotRecognized|')
+    for key, val in __codex.items():
+        if val == value:
+            return key
 
 def get_value_from_key(key):
-    if key in __get_keys():
-        return __codex.get(key, None)
+    if key not in __get_keys():
+        return (255,255,255)
+    return __codex.get(key, None)
 
 def encode(input_text:str):
     input_list = list(input_text)
